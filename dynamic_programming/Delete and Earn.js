f0e9fd08f205c38b1698d3166d78b0df762e71ac
Ashish Bansal <ashish.bansal@gaadi.com>
2021-10-04 23:00:42 +0530

    day 3

---
+++ dynamic_programming/Delete and Earn.js
@@ -0,0 +1,58 @@
+/**
+ * You are given an integer array nums. 
+ * You want to maximize the number of points you get by performing the following operation any number of times:
+ * Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 
+ * and every element equal to nums[i] + 1.
+ * Return the maximum number of points you can earn by applying the above operation some number of times.
+ * 
+ * Example 1:
+ *  Input: nums = [3,4,2]
+ *  Output: 6
+ *  Explanation: You can perform the following operations:
+ *  - Delete 4 to earn 4 points. Consequently, 3 is also deleted. nums = [2].
+ *  - Delete 2 to earn 2 points. nums = [].
+ *  You earn a total of 6 points.
+ *  
+ * Example 2:
+ *  Input: nums = [2,2,3,3,3,4]
+ *  Output: 9
+ *  Explanation: You can perform the following operations:
+ *  - Delete a 3 to earn 3 points. All 2's and 4's are also deleted. nums = [3,3].
+ *  - Delete a 3 again to earn 3 points. nums = [3].
+ *  - Delete a 3 once more to earn 3 points. nums = [].
+ *  You earn a total of 9 points.
+ * 
+ * Constraints:
+ *  1 <= nums.length <= 2 * 104
+ *  1 <= nums[i] <= 104
+ */
+
+const deleteAndEarn = function(nums) {
+    let max = 0;
+    
+    if(!nums.length) {
+        return 0;
+    }
+    
+    for(let i=0;i<nums.length;i++) {
+        max = Math.max(nums[i], max);
+    }
+    
+    if(max == 1) {
+        return nums.length;
+    }
+    
+    let occurence = new Array(max+1);
+    occurence.fill(0);
+    
+    for(let i=0;i<nums.length;i++) {
+        let current = nums[i];
+        occurence[current] += current;
+    }
+    
+    for(let i=2; i<occurence.length;i++) {
+        occurence[i] = Math.max(occurence[i-1], occurence[i]+occurence[i-2]);
+    }
+    
+    return occurence[max];
+};
