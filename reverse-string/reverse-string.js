090b656f59deae60bec09578ede041d5a12ed69a
Ashish Bansal <ashishbansal08@gmail.com>
2022-06-08 21:33:21 +0530

    Time: 169 ms, Memory: 49.2 MB - LeetHub

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let middle = Math.floor(s.length/2);
+    let middle = s.length/2;
     for(let i=0;i<middle;i++) {
         let temp = s[i];
         s[i] = s[s.length-i-1];
