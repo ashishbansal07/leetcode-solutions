9e32317d3bcf0412d0b2b8b8ced22221706871a8
Ashish Bansal <ashishbansal08@gmail.com>
2023-07-20 11:44:55 +0530

    Time: 66 ms (47.84%), Space: 44.3 MB (31.97%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -6,7 +6,6 @@ var subsets = function(nums) {
     let result = [[]];
     
     for(let i=0;i<nums.length;i++) {
-        
         let len = result.length;
         for(let j=0; j < len; j++) {
             let curr = result[j].slice(0);
